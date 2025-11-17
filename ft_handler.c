79e76a72e76a2fb427190badd49594907b0b480b
Mohamed-Amine Elammari <moelamma@e1r9p7.1337.ma>
2025-11-17 23:08:13 +0100

    printf

---
+++ ft_handler.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_handler.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moelamma <moelamma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 22:53:59 by moelamma          #+#    #+#             */
+/*   Updated: 2025/11/17 23:03:38 by moelamma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	handle_char(char specifier, va_list args)
+{
+	if (specifier == 'c')
+		return (print_char(va_arg(args, int)));
+	else if (specifier == 's')
+		return (print_str(va_arg(args, char *)));
+	else if (specifier == '%')
+	{
+		write(1, "%", 1);
+		return (1);
+	}
+	return (-1);
+}
+
+static int	handle_numbers(char specifier, va_list args)
+{
+	if (specifier == 'd' || specifier == 'i')
+		return (print_number(va_arg(args, int)));
+	else if (specifier == 'u')
+		return (print_unsigned(va_arg(args, unsigned int)));
+	else if (specifier == 'x')
+		return (print_hex(va_arg(args, unsigned int), 0));
+	else if (specifier == 'X')
+		return (print_hex(va_arg(args, unsigned int), 1));
+	else if (specifier == 'p')
+		return (print_pointer(va_arg(args, void *)));
+	return (-1);
+}
+
+int	handle_specifier(char specifier, va_list args)
+{
+	int	res;
+
+	if (specifier == '\0')
+		return (0);
+	res = handle_char(specifier, args);
+	if (res != -1)
+		return (res);
+	res = handle_numbers(specifier, args);
+	if (res != -1)
+		return (res);
+	write(1, "%", 1);
+	write(1, &specifier, 1);
+	return (2);
+}
--- ft_printf.c
+++ ft_printf.c
@@ -6,42 +6,11 @@
 /*   By: moelamma <moelamma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 22:36:13 by moelamma          #+#    #+#             */
-/*   Updated: 2025/11/17 22:46:14 by moelamma         ###   ########.fr       */
+/*   Updated: 2025/11/17 23:04:07 by moelamma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-int	handle_specifier(char specifier, va_list args)
-{
-	if (specifier == '\0')
-		return (0);
-	else if (specifier == 'c')
-		return (print_char(va_arg(args, int)));
-	else if (specifier == 's')
-		return (print_str(va_arg(args, char *)));
-	else if (specifier == 'd' || specifier == 'i')
-		return (print_number(va_arg(args, int)));
-	else if (specifier == 'u')
-		return (print_unsigned(va_arg(args, unsigned int)));
-	else if (specifier == 'x')
-		return (print_hex(va_arg(args, unsigned int), 0));
-	else if (specifier == 'X')
-		return (print_hex(va_arg(args, unsigned int), 1));
-	else if (specifier == 'p')
-		return (print_pointer(va_arg(args, void *)));
-	else if (specifier == '%')
-	{
-		write(1, "%", 1);
-		return (1);
-	}
-	else
-	{
-		write(1, "%", 1);
-		write(1, &specifier, 1);
-		return (2);
-	}
-}
 
 int	ft_printf(const char *str, ...)
 {
