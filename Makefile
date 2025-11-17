b1d3cd046001fc5ec2b19ef98ea26ad35ba85ee1
Mohamed-Amine Elammari <moelamma@e1r9p7.1337.ma>
2025-11-17 23:25:29 +0100

    add header and Makefile

---
+++ Makefile
@@ -0,0 +1,27 @@
+NAME = libftprintf.a
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+RM = rm -f
+AR = ar rc
+
+SRC = ft_printf.c ft_handler.c ft_cstr.c ft_number.c ft_hex.c
+
+OBJ = $(SRC:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(AR) $(NAME) $(OBJ)
+
+%.o: %.c ft_printf.h
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean: 
+	$(RM) $(OBJ)
+
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean
+
+.PHONY : all clean fclean re
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: moelamma <moelamma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 22:36:46 by moelamma          #+#    #+#             */
-/*   Updated: 2025/11/17 22:45:31 by moelamma         ###   ########.fr       */
+/*   Updated: 2025/11/17 23:14:44 by moelamma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,14 @@
 
 # include <stdarg.h>
 # include <unistd.h>
+
+int	ft_printf(const char *str, ...);
+int	handle_specifier(char specifier, va_list args);
+int	print_char(int c);
+int	print_str(char *s);
+int	print_number(int n);
+int	print_unsigned(unsigned int n);
+int	print_hex(unsigned long n, int uppercase);
+int	print_pointer(void *ptr);
 
 #endif
